486f51cb8781b81c318fee6b637e8cd8cc28ae21
Nguyen Smile <nguyenlc.smile@gmail.com>
2022-12-19 22:01:37 +0700

    dev

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,19 +8,56 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent implements OnInit {
-    email = '';
-    password = '';
+    account = {
+        email: {
+            value: '',
+            isValid: false
+        },
+        password: {
+            value: '',
+            isValid: false
+        }
+    }
+
     showPass = false;
+    messageEmail = '';
 
     constructor(private router: Router) { }
 
-    ngOnInit(): void {}
+    ngOnInit(): void { }
+
+    validateEmail(email) {
+        return String(email).toLowerCase()
+            .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    }
 
     getInformation(event) {
         event.preventDefault();
-        console.log(this.email, this.password);
-        this.email = '';
-        this.password = '';
+
+        // validate form 
+        let nameInput = ['email', 'password']
+        for (let i = 0; i < nameInput.length; i++) {
+            this.account.email.isValid = false;
+            this.account.password.isValid = false;
+
+            if (this.account[nameInput[i]].value === '') {
+                this.account[nameInput[i]].isValid = true;
+                if (nameInput[i] === 'email') this.messageEmail = 'Please enter email.'
+                return;
+            }
+        }
+
+        // validate email
+        let isValidEmail = this.validateEmail(this.account.email.value);
+        if (!isValidEmail) {
+            this.account.email.isValid = true;
+            this.messageEmail = 'Email is not valid.'
+            return;
+        }
+
+        console.log(this.account.email.value, this.account.email.value);
+        this.account.email.value = '';
+        this.account.password.value = '';
     }
 
     showPassword() {
